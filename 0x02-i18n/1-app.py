eb0c08c5260fba1ad57e08919e2d46b001a49b97
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-28 15:23:48 +0100

    Done with task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,7 +11,7 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
 
 app = Flask(__name__)
 app.config.from_object(Config)

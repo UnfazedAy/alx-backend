9b2fae0c79226db4ef817615dcf5c1a30efd2c3c
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-28 08:51:55 +0100

    Executed the job queue using Kue Job process

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -8,7 +8,7 @@ const data = {
 }
 
 const job = queue.create('push_notification_code', data).save((err) => {
-  if (!err) console.log('Notification job created: ', job.id);
+  if (!err) console.log('Notification job created:', job.id);
 });
 
 job.on('complete', () => console.log('Notification job completed'));
